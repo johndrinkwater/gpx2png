85bcfbc829ce7a00840bf7d9aa9d17fd022ee35b
John Drinkwater <john@nextraweb.com>
2010-11-01 13:34:18 +0000

    Start work on calculating the tiles required. This will help us decide on the scale and bounding box.

--- gpx2png.py
+++ gpx2png.py
@@ -1,8 +1,9 @@
-#/usr/bin/python
+#! /usr/bin/python
 # -*- coding: utf-8 -*-
-
 # GPLv3, John Drinkwater <john@nextraweb.com>
 # http://johndrinkwater.name/code/gpx2png/
+
+import math
 
 # defaults
 ofilename = "map.png"
@@ -39,6 +40,50 @@ tileserver = 'http://tile.openstreetmap.org'
 
 # XXX we are just using defaults now
 
+#### Helper classes here
+
+class Tile:
+	
+	# Code from OSM
+	@staticmethod
+	def getNumber( lat, long, zoom ):
+		latrad = math.radians(lat)
+		n = 2.0 ** zoom
+		xtile = int((long + 180.0) / 360.0 * n)
+		ytile = int((1.0 - math.log(math.tan(latrad) + (1 / math.cos(latrad))) / math.pi) / 2.0 * n)
+		return(xtile, ytile)
+
+	# Code from OSM
+	@staticmethod
+	def getCoords( xtile, ytile, zoom ):
+		n = 2.0 ** zoom
+		long = xtile / n * 360.0 - 180.0
+		latrad = math.atan(math.sinh(math.pi * (1 - 2 * ytile / n)))
+		lat = math.degrees(latrad)
+		return(lat, long)
+
 # TODO read file
 
 # assuming we track data here, loop to calc tiles needed
+def calculateTiles( points ):
+	tilexmin = 2^20
+	tilexmax = 0
+	tileymin = 2^20
+	tileymax = 0
+
+	zoom = 10
+
+	for point in points:
+		[xtile, ytile] = Tile.getNumber( point[0], point[1], zoom )
+		tilexmin = min(xtile, tilexmin)
+		tilexmax = max(xtile, tilexmax)
+		tileymin = min(ytile, tileymin)
+		tileymax = max(ytile, tileymax)
+
+	# now whittle it down to osize = max
+	# TODO where to expand tiles for cropping?
+	
+# Just test data for now
+print calculateTiles( [[51.937697,-1.983817],
+	[51.947704,-1.973797],
+	[51.997670,-1.923847]])
