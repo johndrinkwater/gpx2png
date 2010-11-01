98bd6c850b76c4bb0394657c57d5f370cd71b67f
John Drinkwater <john@nextraweb.com>
2010-11-01 15:12:33 +0000

    Completely rework structure of process, place it all into Tile class for now.

--- gpx2png.py
+++ gpx2png.py
@@ -20,6 +20,12 @@ border = 50
 # TODO allow for wider/taller output images?
 osize = 2
 
+# Compute zoom for us, use osize
+autozoom = True
+
+# default if not auto..
+zoom = 10
+
 # need to include CC notice if we use tiles
 cnotice = "CC BY-SA OpenStreetMap"
 
@@ -44,46 +50,80 @@ tileserver = 'http://tile.openstreetmap.org'
 
 class Tile:
 	
-	# Code from OSM
 	@staticmethod
 	def getNumber( lat, long, zoom ):
+		# Code from OSM
 		latrad = math.radians(lat)
 		n = 2.0 ** zoom
 		xtile = int((long + 180.0) / 360.0 * n)
 		ytile = int((1.0 - math.log(math.tan(latrad) + (1 / math.cos(latrad))) / math.pi) / 2.0 * n)
-		return(xtile, ytile)
+		return (xtile, ytile)
 
-	# Code from OSM
 	@staticmethod
 	def getCoords( xtile, ytile, zoom ):
+		# Code from OSM
 		n = 2.0 ** zoom
 		long = xtile / n * 360.0 - 180.0
 		latrad = math.atan(math.sinh(math.pi * (1 - 2 * ytile / n)))
 		lat = math.degrees(latrad)
 		return(lat, long)
 
+	@staticmethod
+	def getTileURL( xtile, ytile, zoom ):
+		return '/'.join( [tileserver, str(zoom), str(xtile), str(ytile)] ) + '.png'
+
+#	@staticmethod
+#	def quickTest( tiles ):
+#
+#		for x in range(tiles['x']['min'],tiles['x']['min'] + tiles['x']['count'] + 1):
+#			for y in range(tiles['y']['min'],tiles['y']['min'] + tiles['y']['count'] + 1):
+#				print Tile.getTileURL( x, y, tiles['zoom'] )
+
+	# returns tile bounding box for the points at this zoom level
+	# Be cautious, as this can produce a lot of tiles
+	# The BB is +1 in all directions so we can trim the image later ()
+	@staticmethod
+	def calculateTiles( points, zoom = 10 ):
+
+		tilexmin = tileymin = 200000
+		tilexmax = tileymax = 0
+		
+		for point in points:
+			[xtile, ytile] = Tile.getNumber( point[0], point[1], zoom )
+			tilexmin = min(xtile, tilexmin)
+			tilexmax = max(xtile, tilexmax)
+			tileymin = min(ytile, tileymin)
+			tileymax = max(ytile, tileymax)
+
+		# TODO possibly expand here wrt image ‘border’
+
+		return {'x': { 'min':tilexmin - 1, 'max':tilexmax + 1, 'count': tilexmax - tilexmin + 3},
+				'y': { 'min':tileymin - 1, 'max':tileymax + 1, 'count': tileymax - tileymin + 3},
+				'zoom': zoom }
+
+	# returns tile bounding box that is automatically scaled to a correct zoom level.
+	# The BB is +1 in all directions so we can trim the image later ()
+	@staticmethod
+	def calculateTilesAuto( points ):
+
+		zoomdefault = 16
+
+		# get the default scale tiles
+		tiles = Tile.calculateTiles( points, zoomdefault )
+
+		while ( (tiles['x']['count']+1) * (tiles['y']['count']+1) > (osize * osize) ):
+			zoomdefault -= 1
+			tiles['x']['count'] >>= 1
+			tiles['y']['count'] >>= 1
+
+		# get the re-scaled tiles
+		return Tile.calculateTiles( points, zoomdefault )
+
+
 # TODO read file
 
-# assuming we track data here, loop to calc tiles needed
-def calculateTiles( points ):
-	tilexmin = 2^20
-	tilexmax = 0
-	tileymin = 2^20
-	tileymax = 0
-
-	zoom = 10
-
-	for point in points:
-		[xtile, ytile] = Tile.getNumber( point[0], point[1], zoom )
-		tilexmin = min(xtile, tilexmin)
-		tilexmax = max(xtile, tilexmax)
-		tileymin = min(ytile, tileymin)
-		tileymax = max(ytile, tileymax)
-
-	# now whittle it down to osize = max
-	# TODO where to expand tiles for cropping?
-	
 # Just test data for now
-print calculateTiles( [[51.937697,-1.983817],
+tiles = Tile.calculateTilesAuto( [[51.937697,-1.983817],
 	[51.947704,-1.973797],
-	[51.997670,-1.923847]])
+	[51.997670,-1.923847]] )
+
