3e0bf2c24ba3103f5ef5d9b23f18bc0d4349d7a3
John Drinkwater <john@nextraweb.com>
2010-11-01 13:02:02 +0000

    Some variables, nothing more.

--- gpx2png.py
+++ gpx2png.py
@@ -1,3 +1,44 @@
 #/usr/bin/python
 # -*- coding: utf-8 -*-
 
+# GPLv3, John Drinkwater <john@nextraweb.com>
+# http://johndrinkwater.name/code/gpx2png/
+
+# defaults
+ofilename = "map.png"
+
+verbose = False
+
+# use tiles from OSM
+background = True
+
+# px border around track
+border = 50
+
+# Max tile w×h for output image, we auto scale
+# TODO allow for wider/taller output images?
+osize = 2
+
+# need to include CC notice if we use tiles
+cnotice = "CC BY-SA OpenStreetMap"
+
+# TODO caching?
+# TODO text overlays
+# TODO SVG graphics for cnotice
+# TODO options for tiles renderer
+
+# variables
+version = 0.01
+
+
+# TODO parse cli here
+tilerenderer = 'mapnik'
+tileserver = 'http://tile.openstreetmap.org'
+
+# TODO if Verbose, output parsed options
+
+# XXX we are just using defaults now
+
+# TODO read file
+
+# assuming we track data here, loop to calc tiles needed
