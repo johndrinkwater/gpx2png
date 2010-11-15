06b8b523898528ea6953ade86b6614f9b9ef2af0
John Drinkwater <john@nextraweb.com>
2010-11-15 15:36:44 +0000

    Add COPYING, which includes GPLv3 text, and add the small blurb at the top of the .py

--- gpx2png.py
+++ gpx2png.py
@@ -1,8 +1,21 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
 """
- GPLv3, John Drinkwater <john@nextraweb.com>
- http://johndrinkwater.name/code/gpx2png/
+	© 2010 John Drinkwater <john@nextraweb.com>
+	http://johndrinkwater.name/code/gpx2png/
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 """
 
